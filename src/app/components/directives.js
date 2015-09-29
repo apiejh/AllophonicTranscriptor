b6747dc070b58b6d4b3e33cd63d736a4e51ecaec
Neducatio Web Producers <gaperlinski@n-educatio.pl>
2015-09-29 15:07:16 +0200

    wip

--- src/app/components/directives.js
+++ src/app/components/directives.js
@@ -7,14 +7,36 @@
 
   /** @ngInject */
   function keyboard() {
-  	return {
-  		restrict: 'E',
-	    replace: 'true',
-	    templateUrl: 'app/components/keyboard.template.html',
-	    scope: {},
-	    link: function($scope, $elem, $attr) {
+    return {
+      restrict: 'E',
+      replace: 'true',
+      templateUrl: 'app/components/partials/keyboard.template.html',
+      scope: {},
+      link: function($scope, $elem, $attr) {
 
-		}
-	};
+    }
+  };
+  }
+
+})();
+
+(function() {
+  'use strict';
+
+  angular
+    .module('AllophonicTranscriptor')
+    .directive('settings', settings);
+
+  /** @ngInject */
+  function settings() {
+    return {
+      restrict: 'E',
+      replace: 'true',
+      templateUrl: 'app/components/partials/settings.template.html',
+      scope: {},
+      link: function($scope, $elem, $attr) {
+
+    }
+  };
   }
 })();
